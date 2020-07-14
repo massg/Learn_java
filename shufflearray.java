6ffa000d4a85ace9ae8a6d58f379a42dddcbc6b3
massg <sreesruthipk@gmail.com>
2020-07-14 20:16:06 +0530

    shuffle the array

---
+++ shufflearray.java
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/shuffle-the-array/
+
+//Solution of the problem is given; main() is not provided ; Note the class name is also not the saame
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int len = nums.length;
+        int[] temp = new int[len];
+        for(int i=0,j=0,k=n;i<len;i++){
+            if(i%2 == 0)
+                temp[i] = nums[j++];
+            else
+                temp[i] = nums[k++];
+        }
+        return temp;
+    }
+}
