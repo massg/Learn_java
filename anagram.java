ab98e45e2586facf77dc19f0f64b9036304a9b22
massg <sreesruthipk@gmail.com>
2020-08-02 10:25:18 +0530

    valid anagram

---
+++ anagram.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        if( s=="" && t == "") return true;
+        HashMap<Character,Integer> hash1 = new HashMap<Character,Integer>();
+        HashMap<Character,Integer> hash2 = new HashMap<Character,Integer>();
+        char[] s1 = s.toCharArray();
+        for(char i:s1){
+            if(hash1.containsKey(i))
+                hash1.put(i,hash1.getOrDefault(i,0)+1);
+        }
+        char[] s2 = t.toCharArray();
+        for(char i:s2){
+            if(hash2.containsKey(i))
+                hash2.put(i,hash1.getOrDefault(i,0)+1);
+        }
+        if(hash1.equals(hash2)) return true;
+        return false;
+    }
+}
