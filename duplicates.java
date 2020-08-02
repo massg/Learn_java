87c43f33684d31e648238470e792f541b82da762
massg <sreesruthipk@gmail.com>
2020-08-02 10:40:15 +0530

    duplicates in array

---
+++ duplicates.java
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet();
+        for(int i:nums){
+            set.add(i);
+        }
+        if(set.size() == nums.length) return false;
+
+        return true;
+
+    }
+}
