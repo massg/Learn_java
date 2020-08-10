6492948f7bb2298b1f02d2deec19617952121d1e
massg <sreesruthipk@gmail.com>
2020-08-10 18:00:29 +0530

    Largest increasing sequence

---
+++ largestincreasingseq.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class largestincreasingseq{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    int n= s.nextInt();
+    int ar[] = new int[n];
+    int dp[] = new int[n];
+    for(int i=0;i<n;i++){
+      ar[i] = s.nextInt();
+      dp[i] = 1;
+     }
+     for(int i=0;i<n;i++){
+       for(int i=0;j<i;i++){
+         if(ar[j]<=ar[i]){
+           if(dp[i]+1>dp[i]) dp[i] = dp[j]+1;
+         }
+       }
+     }
+     System.out.println(dp[n-1]);
+  }
+}
